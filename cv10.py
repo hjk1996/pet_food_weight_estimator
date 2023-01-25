4b165f19da85f966a6f91ed041873b7c8241c40f
khj <dunhill741@naver.com>
2023-01-25 18:04:48 +0900

    edit cv10 summary logic

--- cv10.py
+++ cv10.py
@@ -4,6 +4,8 @@ import argparse
 from typing import List
 from datetime import datetime
 
+
+from glob import glob
 import numpy as np
 import torch
 import torch.nn as nn
@@ -16,6 +18,38 @@ from augmentation import kim_aug
 from models.model_loader import make_model
 
 
+def summarize_results(src: str, dst: str) -> None:
+    log_name = []
+    highest_accs = []
+    highest_f1s = []
+    lowest_rmses = []
+
+    paths = glob(f"{src}/*.csv")
+
+    for path in paths:
+        df = pd.read_csv(path)
+        log_name.append(path.split("/")[-1])
+        highest_accs.append(df["val_acc"].max())
+        highest_f1s.append(df["val_f1"].max())
+        lowest_rmses.append(df["val_rmse"].min())
+
+    mean_highest_acc = np.mean(highest_accs)
+    mean_highest_f1 = np.mean(highest_f1s)
+    mean_lowest_rmse = np.mean(lowest_rmses)
+
+    summary = pd.DataFrame(
+        {
+            "log_name": log_name,
+            "highest_acc": highest_accs,
+            "highest_f1": highest_f1s,
+            "lowest_rmse": lowest_rmses,
+            "mean_highest_acc": [mean_highest_acc],
+            "mean_highest_f1": [mean_highest_f1],
+            "mean_lowest_rmse": [mean_lowest_rmse],
+        }
+    )
+    summary.to_csv(dst, index=False)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -26,7 +60,9 @@ if __name__ == "__main__":
         help="훈련에 사용할 설정이 정의되어 있는 json file의 경로",
     )
     parser.add_argument(
-        "--device", type=str, default="cuda",
+        "--device",
+        type=str,
+        default="cuda",
     )
     parser.add_argument("--test_mode", action="store_true")
     parser.add_argument("--start_fold", type=int, default=0)
@@ -71,12 +107,7 @@ if __name__ == "__main__":
             test_mode=args.test_mode,
         )
 
-    folds = []
-    lowest_rmses = []
-    highest_accs = []
-    highest_f1s = []
-
-
+    cv10_log_path = os.path.join(train_config.save_path, "cv10_log")
     for fold, dataloaders in enumerate(dataset_list):
         if fold < args.start_fold:
             continue
@@ -95,30 +126,9 @@ if __name__ == "__main__":
             targer_acc=train_config.target_acc,
             targer_f1=train_config.target_f1,
         )
-        folds.append(fold)
-        lowest_rmses.append(df["rmse"].min())
-        highest_accs.append(df["acc"].max())
-        highest_f1s.append(df["f1"].max())
+        df.to_csv(
+            os.path.join(cv10_log_path, f"fold{fold}.csv"),
+            index=False,
+        )
 
-    cv10_summary = pd.DataFrame(
-        {
-            "fold": folds,
-            "lowest_rmse": lowest_rmses,
-            "highest_acc": highest_accs,
-            "highest_f1": highest_f1s,
-            "mean_lowest_rmse": np.mean(lowest_rmses) ,
-            "mean_highest_acc": np.mean(highest_accs) ,
-            "mean_highest_f1": np.mean(highest_f1s) ,
-        }
-    )
-
-
-    cv10_log_folder_path = os.path.join(train_config.save_path, "cv10_log")
-    os.makedirs(cv10_log_folder_path, exist_ok=True)
-    cv10_summary.to_csv(os.path.join(cv10_log_folder_path, "cv10_summary.csv"), index=False)
-    end_time = datetime.now()
-    print(f"cv10.py finished at {end_time}")
-
-
-
-
+    summarize_results(cv10_log_path, os.path.join(cv10_log_path, "cv10_summary.csv"))
